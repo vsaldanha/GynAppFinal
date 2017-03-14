c63478a056b5e7a0066378a88c767a5bdf619452
Vanita <Vanita>
2017-03-14 10:20:43 +0530

    Committing entire project structure

--- Gyn/WebContent/js/app.js
+++ Gyn/WebContent/js/app.js
@@ -1,4 +1,5 @@
-var routingApp = angular.module('routingApp', [ 'ngRoute', 'ui.grid', 'ui.grid.pagination' ]);
+var routingApp = angular.module('routingApp', [ 'ngRoute', 'ui.grid',
+		'ui.grid.pagination' ]);
 
 console.log("Entered app.js")
 
@@ -11,6 +12,9 @@ routingApp.config([ '$routeProvider', '$locationProvider',
 			}).when('/grid', {
 				templateUrl : 'Gyn/pages/grid.html',
 				controller : 'GridController'
+			}).when('/tabs', {
+				templateUrl : 'Gyn/pages/tabular_forms.html',
+				controller : 'TabController'
 			}).otherwise({
 				redirectTo : '/index'
 			});
@@ -20,81 +24,45 @@ routingApp.config([ '$routeProvider', '$locationProvider',
 			})
 		} ]);
 
-routingApp.controller("uigridCtrl", function($scope) {
-	$scope.gridOptions = {
-		enableFiltering : true,
-		enablePagination: true,
-		paginationPageSizes: [5, 10, 15],
-		paginationPageSize: 5,
-		onRegisterApi : function(gridApi) {
-			$scope.grid1Api = gridApi;
-		}
-	};
-	$scope.users = [ {
-		ur_number : 154104,
-		surname : 'Bortignon',
-		given_name : 'Maureen Joy',
-		date_of_birth : '21/10/1951',
-		first_presented_diagnosis : 'Uterus'
-	}, {
-		ur_number : 158875,
-		surname : 'Cumming',
-		given_name : 'Tahnee',
-		date_of_birth : '09/02/1976',
-		first_presented_diagnosis : 'Cervix'
-	}, {
-		ur_number : 151233,
-		surname : 'Simpkin',
-		given_name : 'Dorothy May',
-		date_of_birth : '01/05/1974',
-		first_presented_diagnosis : 'Vulva'
-	}, {
-		ur_number : 167890,
-		surname : 'Walters',
-		given_name : 'Margaret Anne',
-		date_of_birth : '10/10/1988',
-		first_presented_diagnosis : 'Ovary'
-	}, {
-		ur_number : 134569,
-		surname : 'De Bruijne',
-		given_name : 'Elizabeth',
-		date_of_birth : '16/04/1969',
-		first_presented_diagnosis : 'Fallopian Tube'
-	}, {
-		ur_number : 151109,
-		surname : 'Poland',
-		given_name : 'Rose',
-		date_of_birth : '28/03/1959',
-		first_presented_diagnosis : 'Uterus'
-	}, {
-		ur_number : 153567,
-		surname : 'Bishop',
-		given_name : 'Mary',
-		date_of_birth : '09/02/1976',
-		first_presented_diagnosis : 'Cervix'
-	}, {
-		ur_number : 134657,
-		surname : 'Lees',
-		given_name : 'Alison',
-		date_of_birth : '01/05/1974',
-		first_presented_diagnosis : 'Vulva'
-	}, {
-		ur_number : 166752,
-		surname : 'Vaega',
-		given_name : 'Voice',
-		date_of_birth : '30/09/1980',
-		first_presented_diagnosis : 'Ovary'
-	}, {
-		ur_number : 145678,
-		surname : 'Jacobs',
-		given_name : 'Robyn',
-		date_of_birth : '16/03/1986',
-		first_presented_diagnosis : 'Ovary'
-	},
+routingApp.controller(
+				"uigridCtrl",
+				function($scope, $http) {
+					$scope.gridOptions = {
+						enableFiltering : true,
+						enablePagination : true,
+						data : 'data',
+						paginationPageSizes : [ 5, 10, 15 ],
+						paginationPageSize : 5,
+						onRegisterApi : function(gridApi) {
+							$scope.grid1Api = gridApi;
+						}
+					};
 
-	];
-	$scope.gridOptions.data = $scope.users;
-});
+					$scope.gridOptions.columnDefs = [
+							{
+								field : 'ur_number',
+								cellTemplate : '<div><a href="/tabs">{{row.entity.ur_number}}</a></div>'
+							}, {
+								field : 'surname'
+							}, {
+								field : 'given_name'
+							}, {
+								field : 'date_of_birth'
+							}, {
+								field : 'first_presented_diagnosis'
+							} ];
+
+					$http.get('Gyn/mockJSON/mock.json').then(successCallback,
+							errorCallback);
+					function successCallback(data) {
+						console.log(data.data);
+						$scope.data = data.data;
+					}
+					function errorCallback(error) {
+						console.log("AJAX call error");
+					}
+
+				});
 
 /* Controller for Homepage */
 routingApp.controller('HomeController', function($scope) {
@@ -103,10 +71,27 @@ routingApp.controller('HomeController', function($scope) {
 });
 
 /* Controller for Grid */
-routingApp
-		.controller(
+routingApp.controller(
 				'GridController',
 				function($scope) {
 					console.log("grid controller executed");
 					$scope.message = 'Message from Controller B. Using Routing showing in Template B.';
+				});
+
+/* Controller for displaying tabular forms */
+routingApp.controller(
+				'TabController',
+				function($scope, $window, $http) {
+					console.log("tab controller executed");
+					
+					$http.get('Gyn/mockJSON/PatientDetails.json').then(successCallback,
+							errorCallback);
+					function successCallback(data) {
+						console.log(data.data);
+						$scope.data = data.data;
+					}
+					function errorCallback(error) {
+						console.log("AJAX call error");
+					}
+					$window.location.href="http://localhost:8080/Gyn/pages/tabular_forms.html";
 				});
